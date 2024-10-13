8c375ebdae1d840fe1435ced40562b4865a157a2
MMbats <petermbatia657@gmail.com>
2024-10-13 03:14:16 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+// index.js
+const name = "Susan"; // Set name to "Susan" to match the test requirement
+const height = 30;    // Set height to a value less than 40
+const message = `${name} is ${height} inches tall`; // Create a message using string interpolation
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
